c64b37d819f2efdea8ac384a7a7c0220a274ea84
anu <anub018@gmail.com>
2018-05-13 12:49:31 +0530

    added recyclerView

--- app/src/main/java/com/anubansal/rvvisibility/MainActivity.java
+++ app/src/main/java/com/anubansal/rvvisibility/MainActivity.java
@@ -3,16 +3,59 @@ package com.anubansal.rvvisibility;
 import android.databinding.DataBindingUtil;
 import android.os.Bundle;
 import android.support.v7.app.AppCompatActivity;
+import android.support.v7.widget.LinearLayoutManager;
 
 import com.anubansal.rvvisibility.databinding.ActivityMainBinding;
+
+import java.util.ArrayList;
 
 public class MainActivity extends AppCompatActivity {
 
     ActivityMainBinding mBinding;
+    MyAdapter adapter;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         mBinding = DataBindingUtil.setContentView(this, R.layout.activity_main);
+        ArrayList<String> dataList = initializeArrayList();
+        adapter = new MyAdapter(dataList);
+        mBinding.recyclerView.setLayoutManager(new LinearLayoutManager(this));
+        mBinding.recyclerView.setAdapter(adapter);
+    }
+
+    private ArrayList<String> initializeArrayList() {
+        ArrayList<String> list = new ArrayList<>();
+        list.add("1");
+        list.add("2");
+        list.add("3");
+        list.add("4");
+        list.add("5");
+        list.add("6");
+        list.add("7");
+        list.add("8");
+        list.add("9");
+        list.add("10");
+        list.add("11");
+        list.add("12");
+        list.add("13");
+        list.add("14");
+        list.add("15");
+        list.add("16");
+        list.add("17");
+        list.add("18");
+        list.add("19");
+        list.add("20");
+        list.add("21");
+        list.add("22");
+        list.add("23");
+        list.add("24");
+        list.add("25");
+        list.add("26");
+        list.add("27");
+        list.add("28");
+        list.add("29");
+        list.add("30");
+        return list;
     }
 }
--- app/src/main/java/com/anubansal/rvvisibility/MyAdapter.java
+++ app/src/main/java/com/anubansal/rvvisibility/MyAdapter.java
@@ -1,4 +1,49 @@
 package com.anubansal.rvvisibility;
 
-public class MyAdapter {
+import android.support.annotation.NonNull;
+import android.support.v7.widget.RecyclerView;
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
+import android.widget.LinearLayout;
+import android.widget.TextView;
+
+import java.util.ArrayList;
+import java.util.List;
+
+public class MyAdapter extends RecyclerView.Adapter<MyAdapter.MyViewHolder> {
+
+    List<String> dataList;
+
+    public MyAdapter(ArrayList<String> dataList) {
+        this.dataList = dataList;
+    }
+
+    @NonNull
+    @Override
+    public MyViewHolder onCreateViewHolder(@NonNull ViewGroup parent, int viewType) {
+        return new MyViewHolder(LayoutInflater.from(parent.getContext()).inflate(R.layout.list_item, parent, false));
+    }
+
+    @Override
+    public void onBindViewHolder(@NonNull MyViewHolder holder, int position) {
+        holder.textView.setText(dataList.get(position));
+    }
+
+    @Override
+    public int getItemCount() {
+        return dataList.size();
+    }
+
+    public class MyViewHolder extends RecyclerView.ViewHolder {
+
+        LinearLayout parent;
+        TextView textView;
+
+        public MyViewHolder(View view) {
+            super(view);
+            parent = view.findViewById(R.id.list_item_parent);
+            textView = view.findViewById(R.id.list_item_tv);
+        }
+    }
 }
