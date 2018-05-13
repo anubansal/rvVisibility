2fa0abae1f3142da4283365b2b54a99d5be9cad9
anu <anub018@gmail.com>
2018-05-13 13:09:58 +0530

    added scroll state change listener to keep a tab on current position

--- app/src/main/java/com/anubansal/rvvisibility/MainActivity.java
+++ app/src/main/java/com/anubansal/rvvisibility/MainActivity.java
@@ -1,9 +1,12 @@
 package com.anubansal.rvvisibility;
 
 import android.databinding.DataBindingUtil;
+import android.os.Build;
 import android.os.Bundle;
 import android.support.v7.app.AppCompatActivity;
 import android.support.v7.widget.LinearLayoutManager;
+import android.util.Log;
+import android.view.View;
 
 import com.anubansal.rvvisibility.databinding.ActivityMainBinding;
 
@@ -11,8 +14,11 @@ import java.util.ArrayList;
 
 public class MainActivity extends AppCompatActivity {
 
+    private static final String TAG = MainActivity.class.getSimpleName();
+
     ActivityMainBinding mBinding;
     MyAdapter adapter;
+    LinearLayoutManager layoutManager;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -20,12 +26,27 @@ public class MainActivity extends AppCompatActivity {
         mBinding = DataBindingUtil.setContentView(this, R.layout.activity_main);
         ArrayList<String> dataList = initializeArrayList();
         adapter = new MyAdapter(dataList);
-        mBinding.recyclerView.setLayoutManager(new LinearLayoutManager(this));
+        layoutManager = new LinearLayoutManager(this);
+        mBinding.recyclerView.setLayoutManager(layoutManager);
         mBinding.recyclerView.setAdapter(adapter);
+        layoutManager.findFirstVisibleItemPosition();
+        layoutManager.findLastVisibleItemPosition();
+//        for now, making it work on ver. >= 23
+        if (Build.VERSION.SDK_INT >= 23)
+            mBinding.recyclerView.setOnScrollChangeListener(onScrollChangeListener);
     }
+
+    private View.OnScrollChangeListener onScrollChangeListener = new View.OnScrollChangeListener() {
+        @Override
+        public void onScrollChange(View view, int i, int i1, int i2, int i3) {
+            Log.d(TAG, "first visible position : " + layoutManager.findFirstVisibleItemPosition());
+            Log.d(TAG, "last visible position : " + layoutManager.findLastVisibleItemPosition());
+        }
+    };
 
     private ArrayList<String> initializeArrayList() {
         ArrayList<String> list = new ArrayList<>();
+        list.add("0");
         list.add("1");
         list.add("2");
         list.add("3");
